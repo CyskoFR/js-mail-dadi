4eb33896c54fb95296f7861ff22b0fcc48a732e3
CyskoFR <franc.sko91@gmail.com>
2022-05-03 18:43:46 +0200

    add for if method

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,8 +1,18 @@
 const mail = prompt("Inserisci mail")
 const list = ["boolean@mail.com", "mimmo@mail.com", "pinco@mail.com", "palla@mail.com"]
 
-if (list.includes(mail)) {
-    alert("login effettuato!");
-} else {
-    alert("mail non registrata!")
+for (let i = 0; i < list.length; i++) {
+    if (mail === list[i]) {
+        alert("login effettuato!");
+    } else {
+        alert("mail non registrata!")
+    }
+    break
 }
+
+
+// if (list.includes(mail)) {
+//     alert("login effettuato!");
+// } else {
+//     alert("mail non registrata!")
+// }
